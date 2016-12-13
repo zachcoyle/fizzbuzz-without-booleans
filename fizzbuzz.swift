849ff0b0c688d7a6aabef3d873d0c5364b8bfe28
Zach Coyle <zach@trabian.com>
2016-12-12 21:09:37 -0500

    golf

--- fizzbuzz.swift
+++ fizzbuzz.swift
@@ -2,6 +2,6 @@ import Foundation
 
 let fb: Array<(Int) -> String> = [ { _ in "fizzbuzz"}, { _ in "buzz"}, { _ in "fizz"}, { "\($0)" } ]
 
-let idx : (Int, Int) -> Int = { (Int(ceil(Double($0.1 % $0.0) / 10)) * ($0.0 - 1)) / 2 }
+let idx: (Int, Int) -> Int = { (Int(ceil(Double($0.1 % $0.0) / 10)) * ($0.0 - 1)) / 2 }
 
-print((1...20).map{ fb[[(3, $0), (5, $0)].map(idx).reduce(0, +)]($0) })
+print((1...100).map{ fb[[(3, $0), (5, $0)].map(idx).reduce(0, +)]($0) })
