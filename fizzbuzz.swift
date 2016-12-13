508619a9173976632ff1af3f46ad79688833d5f7
Zach Coyle <zach@trabian.com>
2016-12-12 21:08:23 -0500

    golf\!

--- fizzbuzz.swift
+++ fizzbuzz.swift
@@ -1,18 +1,7 @@
 import Foundation
 
-let fb: Dictionary<Int, (Int) -> String> = [
-    4: {_ in "fizz"}, 
-    2: {_ in "buzz"},
-    0: {_ in "fizzbuzz"},
-    6: { "\($0)" }
-]
+let fb: Array<(Int) -> String> = [ { _ in "fizzbuzz"}, { _ in "buzz"}, { _ in "fizz"}, { "\($0)" } ]
 
-func lookupValue(num: Int) -> Int {
-    let tmp3 = abs(Int(ceil(abs(Double(num).remainder(dividingBy: 3)) / 10))) * 2
-    let tmp5 = abs(Int(ceil(abs(Double(num).remainder(dividingBy: 5)) / 10))) * 4
-    return tmp3 + tmp5
-}
+let idx : (Int, Int) -> Int = { (Int(ceil(Double($0.1 % $0.0) / 10)) * ($0.0 - 1)) / 2 }
 
-let result = (1...100).map{ fb[lookupValue(num: $0)]!($0) }
-
-print(result)
+print((1...20).map{ fb[[(3, $0), (5, $0)].map(idx).reduce(0, +)]($0) })
